56bb61507495e76c28a7d2d91322b1fdb909f49f
thanhhnguyen23 <46698102+thanhhnguyen23@users.noreply.github.com>
2021-12-06 22:39:59 -0500

    example of simple observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+  // example of simple observable
+  observable$!: Observable<any>;
+
+  ngOnInit(){
+    this.observable$ = new Observable((observer) => {
+      observer.next(1);
+      observer.next(2);
+      observer.next(3);
+      observer.complete();
+    });
+
+    this.observable$.subscribe(
+      (value: any) => console.log(value),
+      err => {},
+      () => console.log('this is the end =(')
+    );
+  }
 }
