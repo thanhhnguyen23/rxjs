5bd9f4e4c1b8636af0a294b51182424ad958277c
thanhhnguyen23 <46698102+thanhhnguyen23@users.noreply.github.com>
2021-12-07 21:18:48 -0500

    add observable to a click event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,16 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { interval, fromEvent } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { Component, } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  template:`
-    <button #counterBtn type="button">Click here</button>
-    <h1>Counter: {{ counter }}</h1>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // 'counterBtn' is binded to #counterBtn in html to capture MouseEvent
-  @ViewChild('counterBtn', { static: true, read: ElementRef}) counterBtnRef!: ElementRef;
-
-  counter: number = 0;
 
   ngOnInit(){
-    // listening for button click events inside the DOM
-    fromEvent(this.counterBtnRef.nativeElement, 'click')
-    // switchmap() will reset the current stream and starts a new MouseEvent stream when clicked
-    .pipe(switchMap((_event) => {
-      // outputs the event type, in this case it's MouseEvent
-      console.log(`new event: ${_event}`)
-      // inner observable emitting values
-      return interval(1000)
-    }))
-    .subscribe(data => {
-      // outputs the data stream to console
-      console.log(`data= ${data}`)
-      // saving counter to view layer
-      this.counter = data;
-    });
-
+    // simple observable with a click event
+    fromEvent(document, 'click').subscribe(x => console.log(x))
   }
 
   ngOnDestry(){
